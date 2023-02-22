e68cd87799855c949b5765f54a5fd40eeee185b4
vpescete <valeriopescetellinl@gmail.com>
2023-02-22 12:16:10 +0100

    Fixed make files

--- philo/philo.c
+++ philo/philo.c
@@ -3,17 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpescete <vpescete@42student.fr>           +#+  +:+       +#+        */
+/*   By: vpescete <vpescete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 18:03:54 by vpescete          #+#    #+#             */
-/*   Updated: 2023/02/21 19:44:48 by vpescete         ###   ########.fr       */
+/*   Updated: 2023/02/22 10:12:58 by vpescete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*routine(t_data *data)
+{
+	(void)data;
+	return (0);
+}
+
 int main(int ac, char **av)
 {
+	// pthread_t	tid;
+	// pthread_create(&tid, NULL, &routine, &data);
+
+	// pthread_mutex_t	mutex;
+	// phtread_mutex_init(&mutex, NULL);
 	if (handle_input(ac, av) == 0)
 		return (0);
 	return (0);
--- philo/src/handle_input.c
+++ philo/src/handle_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_input.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpescete <vpescete@42student.fr>           +#+  +:+       +#+        */
+/*   By: vpescete <vpescete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 18:52:24 by vpescete          #+#    #+#             */
-/*   Updated: 2023/02/21 19:44:41 by vpescete         ###   ########.fr       */
+/*   Updated: 2023/02/22 10:14:38 by vpescete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,23 +46,45 @@ void	handle_input_error(void)
 	printf("3th and 4th are consider in milliseconds\e[24m\n\n");
 }
 
-int	handle_input(int ac, char **av)
+void	allocate_data(t_data *data, char **av, char ac)
 {
-	t_data	*data;
-	
-	if (ac < 5 || ac > 6)
-	{
-		handle_input_error();
-		return (0);
-	}
-	data = malloc(sizeof(t_data));
 	if (!data)
-		return (0);
+		return ; // must to handle this error 
 	data->philo_num = ft_atoi(av[1]);
 	data->death_time = ft_atoi(av[2]);
 	data->eat_time = ft_atoi(av[3]);
 	data->sleep_time = ft_atoi(av[4]);
 	if (ac == 6)
 		data->meals_nb = ft_atoi(av[5]);
+	printf("philo_num: %d\n", data->philo_num);
+	printf("death_time: %llu\n", data->death_time);
+	printf("eat_time: %llu\n", data->eat_time);
+	printf("sleep_time: %llu\n", data->sleep_time);
+	if (ac == 6)
+		printf("meals_nb: %d\n", data->meals_nb);
+}
+
+void	allocate_philos(t_philo	*philos, t_data *data)
+{
+	if (!philos)
+		return ;
+	philos->data = data;
+	philos->eat_cont = data->meals_nb;
+
+}
+
+int	handle_input(int ac, char **av)
+{
+	t_data	*data;
+	t_philo	*philos;
+	
+	if (ac < 5 || ac > 6 || !(ft_atoi(av[1]) >= 0 && ft_atoi(av[1]) <= 200))
+	{
+		handle_input_error();
+		return (0);
+	}
+	data = malloc(sizeof(t_data));
+	allocate_data(data, av, ac);
+	philos = malloc(data->philo_num * sizeof(t_philo));
 	return (1);
 }
