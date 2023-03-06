c56eafcaf4c92a12d18119ef0692d1e68acbb14d
vpescete <valeriopescetellinl@gmail.com>
2023-03-06 15:30:23 +0100

    added bonus dir

---
+++ philo_bonus/philo_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpescete <vpescete@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 15:25:36 by vpescete          #+#    #+#             */
+/*   Updated: 2023/03/06 15:29:46 by vpescete         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+# include <semaphore.h>
+# include <string.h>
+
+#endif
